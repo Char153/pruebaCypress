353678d328aff70cd3a37bbe7061f599f216ab83
carlo <csimo0228@gmail.com>
2024-05-15 23:14:59 -0300

    2do commit

--- Cypress/cypress/e2e/1-getting-started/search.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Search test cases',function(){
-    it('Search with results',function(){
-        cy.visit('https://www.automationpractice.pl/');
-        cy.get('#search_query_top').type('Hola que tal tagarnas?');
-        cy.get('[placeholder="Search"]').clear().type('Hola forro').clear().type('Ya paren de boludear');      
-    })
-
-})
